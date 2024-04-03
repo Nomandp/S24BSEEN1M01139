5105453581b435c885a777bb9931408cce47ef55
Nomandp <107404598+Nomandp@users.noreply.github.com>
2024-04-04 02:16:36 +0500

    Exercise6

---
+++ Exercise6.py
@@ -0,0 +1,20 @@
+def print_divisible_by_5(numbers):
+  for num in numbers:
+      if num % 5 == 0:
+          print(num)
+
+numbers = [10, 15, 20, 25, 30, 35, 40, 45, 50]
+print_divisible_by_5(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
