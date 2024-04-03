9164635fd3e21b7eeece2f20c89c83a74cb19fcc
Nomandp <107404598+Nomandp@users.noreply.github.com>
2024-04-04 02:19:07 +0500

    Exercise8

---
+++ Exercise8.py
@@ -0,0 +1,24 @@
+def is_palindrome(num):
+  num_str = str(num)
+  return num_str == num_str[::-1]
+
+num = 12321
+if is_palindrome(num):
+  print("Palindrome")
+elseif:
+  print("Not a Palindrome")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
