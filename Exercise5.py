d4353e24f5ecbf127e0c335974a3013a563fc814
Nomandp <107404598+Nomandp@users.noreply.github.com>
2024-04-04 02:39:46 +0500

    Exercise5.py

--- Exercise5.py
+++ Exercise5.py
@@ -1,12 +1,7 @@
 def check_first_last_same(numbers):
-  if len(numbers) < 1:
-      print("List is empty")
-  elif numbers[0] == numbers[-1]:
-      print("First and Last Numbers are same")
-  else:
-      print("Not Same")
+    print("First and Last Numbers are same" if numbers and numbers[0] == numbers[-1] else "Not Same")
 
-numbers = [1, 2, 3, 4, 5, 6, 1]
+numbers = [0, 2, 3, 4, 5, 6, 1]
 check_first_last_same(numbers)
 
 
